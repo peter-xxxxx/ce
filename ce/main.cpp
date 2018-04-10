c3eb3cc26b953bdab1f254559151c51b8b379336
Peter XU <xsyying@qq.com>
2018-04-09 20:02:24 -0400

    Initial Commit

---
+++ ce/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ce
+//
+//  Created by Peter XU on 2018/4/9.
+//  Copyright © 2018 Peter XU. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
